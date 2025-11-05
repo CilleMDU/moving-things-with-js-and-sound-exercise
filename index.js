ca33c8647f041c0d67fd692194acd442e78a834c
CilleMDU <eaa25cesc@students.eaaa.dk>
2025-11-05 16:56:37 +0100

    Halfway

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Your code here
+"use strict";
+
+const dodger = document.getElementById("dodger");
+
+
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+});
+
+const getMovement = document.getElementById("movement");
+
+const getDeath = document.getElementById("death");
+
+function movingSound() {
+    getMovement.currentTime = 0;
+    getMovement.play();
+}
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    movingSound();
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+    movingSound();
+  }
+}
+
+
+function moveDodgerUp() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom < 360) {
+    dodger.style.bottom = `${bottom + 1}px`;
+    movingSound();
+  }
+}
+
+
+function moveDodgerDown() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 1}px`;
+    movingSound();
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  }
+  if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  }
+});
